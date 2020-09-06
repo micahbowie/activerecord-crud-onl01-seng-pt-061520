8dbadcfd67b59841251776adbc20fead113d33ff
Micah Bowie <micahbowie20@gmail.com>
2020-09-06 17:38:53 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -1,15 +1,9 @@
-# Replace the '__' in the below methods to to make the specs pass!
-# Each '__' corresponds to a single line of code you will need to write.
-# See the example below on the left and how it should look on the right.
-# def make_a_new_movie_instance    # def make_a_new_movie_instance
-#   movie = __                     #   movie = Movie.new
-# end                              # end
+
 
 def can_be_instantiated_and_then_saved
   movie = __
   movie = Movie.new
   movie.title = "This is a title."
-  __
   movie.save
 end
 
@@ -22,7 +16,6 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = __
   movie = Movie.create(attributes)
 end
 
@@ -33,7 +26,6 @@ def can_be_created_in_a_block(args = nil)
   # release_date == 1990
 
   Movie.create do |m|
-    __
     if args == nil
       m.title = "Home Alone"
       m.release_date = 1990
@@ -46,17 +38,14 @@ def can_be_created_in_a_block(args = nil)
 end
 
 def can_get_the_first_item_in_the_database
-  __
   Movie.first
 end
 
 def can_get_the_last_item_in_the_database
-  __
   Movie.last
 end
 
 def can_get_size_of_the_database
-  __
   Movie.count
 end
 
