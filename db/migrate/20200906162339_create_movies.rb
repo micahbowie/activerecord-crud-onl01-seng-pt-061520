319486a8d984bb87e60ec8833c2f736e59d446a8
Micah Bowie <micahbowie20@gmail.com>
2020-09-06 16:27:14 +0000

    Automatically backed up by Learn

--- db/migrate/20200906162339_create_movies.rb
+++ db/migrate/20200906162339_create_movies.rb
@@ -1,5 +1,10 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
-    
+    create_table :movies do |t|
+      t.string :title 
+      t.integer :release_date
+      t.string :title 
+      t.string :title 
+      t.boolean :title 
   end
 end
