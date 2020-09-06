91d54d824b6a557ad4586ca3cb742cb77ae2e8ca
Micah Bowie <micahbowie20@gmail.com>
2020-09-06 16:58:57 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -68,7 +68,7 @@ def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by 
   # release date descending
   
-  Movie.where(first_name: 'Lifo').take
+  Movie.where(release_date: ).take
 end
 
 def can_be_found_updated_and_saved
