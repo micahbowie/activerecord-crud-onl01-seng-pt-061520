58a63b049d4dc1507453140584a5c22b2b23fef8
Micah Bowie <micahbowie20@gmail.com>
2020-09-06 17:00:53 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -68,12 +68,13 @@ def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by 
   # release date descending
   
-  Movie.where(release_date: ).take
+  Movie.where(release_date: >= 2002).take
 end
 
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
   Movie.create(title: "Awesome Flick")
+  Movie.where(title: 'Awesome Flick')
   __
   __
   __
